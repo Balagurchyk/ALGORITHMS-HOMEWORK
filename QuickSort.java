fd4f91ae761426c3dd1b7af84a46f1ddcfa4e457
Balagurchyk <135262924+Balagurchyk@users.noreply.github.com>
2023-12-25 17:25:44 +0100

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -15,14 +15,13 @@ public class QuickSort {
     public static void quickSort(int[] array, int low, int high) {
         if (low < high) {
             int partitionIndex = partition(array, low, high);
-
-            quickSort(array, low, partitionIndex - 1);
+            quickSort(array, low, partitionIndex);
             quickSort(array, partitionIndex + 1, high);
         }
     }
 
     private static int partition(int[] array, int low, int high) {
-        int pivot = array[(low + high) / 2];
+        int pivot = array[low];
         int i = low - 1;
         int j = high + 1;
 
@@ -30,17 +29,23 @@ public class QuickSort {
             do {
                 i++;
             } while (array[i] < pivot);
+
             do {
                 j--;
             } while (array[j] > pivot);
 
             if (i < j) {
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+                swap(array, i, j);
             } else {
                 return j;
             }
         }
     }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
+
